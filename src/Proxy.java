c436b59e92c64afada43913ab9a7714207b5b3d9
Zeroooo-kun <magda.kur01@gmail.com>
2022-06-25 04:16:18 +0200

    improvement

--- src/Proxy.java
+++ src/Proxy.java
@@ -3,7 +3,7 @@ public class Proxy implements Service1 {
 
     @Override
     public void method() {
-        System.out.println("Welcome in our website www.happyday.com !!! We are happy to see you there ^^, We are a newly opened store where you can find every fruit which you need to be HAPPY.");
+        System.out.println("Welcome in our website www.HappyDay.com !!! We are happy to see you there ^^, We are a newly opened store where you can find every fruit which you need to be HAPPY.");
         if (service1 == null)
             service1 = new ServiceImpl();
         service1.method();
